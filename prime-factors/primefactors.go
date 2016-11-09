502ae28ba572377dd884f7123962dbd3547cb74e
Yusuf <snookermad@gmail.com>
2016-11-09 16:33:35 +0000

    refactored prime-factors

--- prime-factors/primefactors.go
+++ prime-factors/primefactors.go
@@ -3,21 +3,15 @@ package prime
 const testVersion = 2
 
 // Factors calculates the factors that multiply to create the prime number
-func Factors(in int64) (res []int64) {
-	if in < 2 {
-		return []int64{}
-	}
-	num := int64(2)
-	for {
-		if in%num == 0 {
-			res = append(res, num)
-			in /= num
+func Factors(in int64) []int64 {
+	res := []int64{}
+	for i := int64(2); in > 1; {
+		if in%i == 0 {
+			res = append(res, i)
+			in /= i
 			continue
 		}
-		if in == 1 {
-			break
-		}
-		num++
+		i++
 	}
-	return
+	return res
 }
